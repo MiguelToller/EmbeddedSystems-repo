1c263be6f3c227231609def536d1920220c6aa89
Luiz Miguel Toller Marconatto <141162499+MiguelToller@users.noreply.github.com>
2026-06-18 18:18:48 -0300

    Update soil_moisture.ino

--- soil_moisture/soil_moisture.ino
+++ soil_moisture/soil_moisture.ino
@@ -3,10 +3,8 @@
 #define RED_LED 4     // LED Vermelho no pino 4
 #define SoilSensor A0 // Sensor de umidade no pino analógico A0
 
-// NOVOS LIMITES AJUSTADOS PARA O SEU SENSOR (Escala de ~600 a 1023)
 #define DRY_THRESHOLD 900  // Acima de 900 é solo Seco (LED Vermelho)
 #define WET_THRESHOLD 750  // Abaixo de 750 é solo bem Úmido (LED Azul/Verde)
-                           // Entre 750 e 900 é solo Moderado (LED Amarelo)
 
 void setup() {
   pinMode(GREEN_LED, OUTPUT);
@@ -27,21 +25,20 @@ void loop() {
   Serial.print("Soil Sensor Value: "); 
   Serial.println(SensorValue); 
 
-  // NOVA LÓGICA DE TRÊS NÍVEIS
   if (SensorValue > 0 && SensorValue <= WET_THRESHOLD) {
-    // Solo Bem Úmido (Agora aceita valores até 750)
+    // Solo Bem Úmido
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(RED_LED, LOW);
   } 
   else if (SensorValue > WET_THRESHOLD && SensorValue <= DRY_THRESHOLD) {
-    // Solo Moderado (Entre 751 e 900)
+    // Solo Moderado
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(YELLOW_LED, HIGH);
     digitalWrite(RED_LED, LOW);
   }
   else if (SensorValue > DRY_THRESHOLD) {
-    // Solo Seco (Acima de 900)
+    // Solo Seco
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(YELLOW_LED, LOW);
     digitalWrite(RED_LED, HIGH);
